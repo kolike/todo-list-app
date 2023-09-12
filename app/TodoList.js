3b726b86d36dd6ded1f6af65b0b017b1e79078fc
Nikolay <kolik_2604@mail.ru>
2023-09-12 12:53:28 +0300

    добавление TodoList

---
+++ app/TodoList.js
@@ -0,0 +1,13 @@
+import TodoListItem from './TodoListItem';
+
+function TodoList() {
+  return (
+    <ul>
+      <li>
+        <TodoListItem />
+      </li>
+    </ul>
+  );
+}
+
+export default TodoList;
