5de81399e933293f798d39ababa35594838c0ba1
Nikolay <kolik_2604@mail.ru>
2023-09-12 20:55:12 +0300

    add functional

--- app/AddTodoListItem.js
+++ app/AddTodoListItem.js
@@ -1,12 +1,44 @@
+'use client';
+
+import { useState } from 'react';
+
 function AddTodoListItem() {
+  const [inputValue, setInputValue] = useState('');
+  const [importantValue, setImportantValue] = useState(false);
+
+  const createNewTask = () => {
+    const newTask = {
+      content: inputValue,
+      important: importantValue,
+    };
+    return newTask;
+  };
+
   return (
     <>
       <div>
-        <input type="text" placeholder="add something..." />
-        <button>Create Task</button>
+        <input
+          required
+          name="name"
+          type="text"
+          placeholder="add something..."
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            console.log('newTask: ', createNewTask());
+          }}
+        >
+          Create Task
+        </button>
         <div>
           important task?
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            value={inputValue}
+            onChange={() => setImportantValue(!importantValue)}
+          />
         </div>
       </div>
     </>
